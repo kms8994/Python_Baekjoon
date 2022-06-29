5cfb253c0d3c5b88357403bd78d7e8fe940b76a8
kms8994 <minsoo8994@gmail.com>
2022-06-29 15:34:15 +0900

    백준  9498번
    시험성적

---
+++ 백준 9498번.py
@@ -0,0 +1,18 @@
+a = input()
+
+a = int(a)
+
+if 90 <= a <= 100:
+    print("A")
+
+elif 80 <= a:
+    print("B")
+
+elif 70 <= a:
+    print("C")
+
+elif 60 <= a:
+    print("D")
+
+else:
+    print("F")
