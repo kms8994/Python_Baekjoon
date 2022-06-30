481783a119eeec528e471c2ca741e339acc139b1
kms8994 <minsoo8994@gmail.com>
2022-06-30 16:12:23 +0900

    백준 14681번
    사분면 구하기

---
+++ 백준 14681번.py
@@ -0,0 +1,17 @@
+x = input()
+y = input()
+
+x = int(x)
+y = int(y)
+
+if x > 0 and y > 0:
+    print("1")
+
+if x < 0 and y > 0:
+    print("2")
+
+if x < 0 and y < 0:
+    print("3")
+
+if x > 0 and y < 0:
+    print("4")
